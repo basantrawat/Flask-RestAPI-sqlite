61170e40da5131d0ce3cdafd440da76a8ea413cc
Basant <basantsrwt@gmail.com>
2020-02-17 03:38:44 +0530

    added the funtion post in /users link

--- app.py
+++ app.py
@@ -12,6 +12,19 @@ class Users(Resource):
         conn = db_connect.connect()
         query = conn.execute("SELECT * FROM users")
         return {'users': [i for i in query.cursor.fetchall()]}
+    
+    def post(self):       
+        user_id = request.form["id"]
+        name = request.form["name"]
+        address = request.form["address"]
+        dob = request.form["dob"]
+        
+        conn = db_connect.connect()
+    
+        query = f"""INSERT INTO users (ID, NAME, ADDRESS, DOB) VALUES ({user_id}, '{name}', '{address}', '{dob}')"""
+        conn.execute(query)
+
+        return ("Inserted Data of {}".format(name))
 
 class Profession(Resource):
     def get(self):
@@ -26,7 +39,12 @@ class UserById(Resource):
         query = conn.execute("select * from users where ID =%d "  %int(id))
         result = {'user': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
         return jsonify(result)
-        
+
+    def delete(self,id):
+        conn = db_connect.connect()
+        conn.execute("DELETE from users where ID =%d "  %int(id))
+        return ("Deleted the User with ID : {}".format(id))
+    
 
 api.add_resource(Users, '/users')
 api.add_resource(Profession, '/profession')
